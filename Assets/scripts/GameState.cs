5ef41a5ee67dba173d00a5deb80c11f096039529
Andrew Gazin <andrewg@Andrew-Gs-MacBook-Pro.local>
2018-01-27 18:39:47 -0800

    Time Update

--- Assets/scripts/GameState.cs
+++ Assets/scripts/GameState.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,14 +12,34 @@ public class GameState : MonoBehaviour
 	
 	public int powerValue;
 	public Text powerText;
+
+	public Text timeText;
+	public float time;
 	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		time = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		//Start Time Update
+		time += Time.deltaTime;
+		int mins = Mathf.FloorToInt(time / 60);
+		int secs = Mathf.FloorToInt(time % 60);
+		string minutes;
+		if (time < 600)
+		{
+			minutes = mins.ToString("0");
+		}
+		else
+		{
+			minutes = mins.ToString("00");
+		}
+		string seconds = secs.ToString("00");
+		timeText.text = minutes + ":" + seconds;
+		//End Time Update
 	}
 }
