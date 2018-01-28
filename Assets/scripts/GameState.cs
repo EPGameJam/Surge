36515aba5814ae06177595e9d31280651e6032dc
Juan Corona <juanc@evidentpoint.com>
2018-01-27 17:14:40 -0800

    New scripts and prefab

---
+++ Assets/scripts/GameState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameState : MonoBehaviour
+{
+
+	public int cashValue;
+	public Text cashText;
+	
+	public int powerValue;
+	public Text powerText;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
