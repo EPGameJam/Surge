436066d7b2ac6320a92d9f2bf0bf75ee7561c643
Andrew Gazin <andrewg@Andrew-Gs-MacBook-Pro.local>
2018-01-28 13:44:14 -0800

    Toggle Destroy Button

---
+++ Assets/SelectTool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+using System;
+using System.ComponentModel;
+using UnityEngine.UI;
+
+public class SelectTool : MonoBehaviour
+{
+	public GameObject Background;
+	public GameObject DestroyButton;
+	public Boolean Toggled;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Toggled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			var MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+			var MouseWorldPos = Camera.main.ScreenToWorldPoint(MousePos);
+			Vector2 MouseVec = new Vector2(MouseWorldPos.x, MouseWorldPos.y);
+			Collider2D toggleButton = DestroyButton.GetComponent<Collider2D>();
+			if (toggleButton.bounds.Contains(MouseVec))
+			{
+				Toggled = !Toggled;
+			}
+		}
+		SpriteRenderer BackgroundRenderer = Background.GetComponent <SpriteRenderer>();
+		if (Toggled)
+		{
+			BackgroundRenderer.enabled = true;
+		}
+		else
+		{
+			BackgroundRenderer.enabled = false;
+		}
+	
+	}
+}
