58cfe83bd69f3dd9d16049df4250720241d6f3ec
Kayci Wang <kayciwang@alumni.ubc.ca>
2018-01-27 17:05:20 -0800

    Boundary check for adding grid

--- Assets/scripts/WireController.cs
+++ Assets/scripts/WireController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 using UnityEngine.Tilemaps;
 
 public class WireController : MonoBehaviour
@@ -10,6 +12,8 @@ public class WireController : MonoBehaviour
 	public Tilemap Tilemap;
 
 	public Tile WireTile;
+
+	public GameObject InvisibleWall;
 	
 	// Use this for initialization
 	void Start () {
@@ -21,8 +25,27 @@ public class WireController : MonoBehaviour
 		if (Input.GetMouseButton(0))
 		{
 			var mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
-			var cellPos = Grid.WorldToCell(Camera.main.ScreenToWorldPoint(mousePos));
-			Tilemap.SetTile(cellPos, WireTile);
+			if (CheckInBoundary(mousePos))
+			{
+				var cellPos = Grid.WorldToCell(Camera.main.ScreenToWorldPoint(mousePos));
+				Tilemap.SetTile(cellPos, WireTile);
+			}
 		}
+	}
+
+	private Boolean CheckInBoundary (Vector3 mousePosition)
+	{
+		var mousePosInWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+		Collider boxCollider = InvisibleWall.GetComponent<Collider>();
+		var boxColliderMaxX = boxCollider.bounds.max.x;
+		var boxColliderMaxY = boxCollider.bounds.max.y;
+		var boxColliderMinX = boxCollider.bounds.min.x;
+		var boxColliderMinY = boxCollider.bounds.min.y;
+		var normalMousePositionX = mousePosInWorld.x;
+		var normalMousePositionY = mousePosInWorld.y;
+		return (normalMousePositionX < boxColliderMaxX 
+		        && normalMousePositionY < boxColliderMaxY
+		        && normalMousePositionX > boxColliderMinX 
+		        && normalMousePositionY > boxColliderMinY);
 	}
 }
