46d5970ce4b0a7d6ecf2a2d340d0021c4717dfa8
Kayci Wang <kayciwang@alumni.ubc.ca>
2018-01-28 16:19:00 -0800

    GameLoader now works

---
+++ Assets/redirectToGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class redirectToGame : MonoBehaviour
+{
+
+	public GameObject StartObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0))
+		{
+			var mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+			var mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			var mousePos2D = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+			Collider2D boxCollide = StartObject.GetComponent<Collider2D>();
+			if (boxCollide.bounds.Contains(mousePos2D))
+			{
+				SceneManager.LoadScene("GameScene");
+			}
+		}
+	}
+}
